6c5f901a71fbaa0b779b634276f93117e7e097d5
sieun1002 <nata20034@naver.com>
2023-11-15 14:13:01 +0900

    카테고리 설정 추가

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   #CBV 형식
   path('', views.PostList.as_view()),
   path('<int:pk>/', views.PostDetail.as_view()),
+  path('category/<str:slug>/', views.category_page),
 
   #FBV 형식
   # path('<int:pk>/', views.single_post_page),
